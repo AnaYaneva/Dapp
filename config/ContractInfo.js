564f1843342c6e4ea53e25597711f0f557efee4f
AnaYaneva <ana_yaneva@abv.bg>
2018-03-06 11:27:33 +0200

    -

---
+++ config/ContractInfo.js
@@ -0,0 +1,20 @@
+function getContractOwner() {
+    let ownerAddress = "";
+    return ownerAddress;
+}
+function getContractABI() {
+    let abi = [
+
+    ];
+    return abi;
+}
+function getContractAddress() {
+    let address = "";
+    return address;
+}
+
+module.exports = {
+    getContractOwner,
+    getContractABI,
+    getContractAddress
+};
---
+++ config/ipfs-config.js
@@ -0,0 +1,8 @@
+function getServerUrl() {
+    let serverUrl = "http://.../ipfs/";
+    return serverUrl;
+}
+
+module.exports = {
+    getServerUrl
+};
